550f3f91ec145a5dbbdcd02d670e772dda9a6b5e
choiminjong <alswhd20@nate.com>
2021-02-28 17:13:49 +0900

    databases 초기 설정후 mysql 연결(insert)

---
+++ app/router/index.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from fastapi import APIRouter, Depends
+from starlette.responses import Response
+from starlette.requests import Request
+from inspect import currentframe as frame
+
+from sqlalchemy.orm import Session
+
+from app.database.conn import db
+from app.database.schema import Users
+router = APIRouter()
+
+
+@router.get("/")
+async def index(session: Session = Depends(db.session),):
+    """
+    ELB 상태 체크용 API
+    :return:
+    """
+    user= Users(status='active', name="HelloWorld")
+    session.add(user)
+    session.commit()
+
+    Users.create(session,auto_commit=True, name="코알라")
+    current_time = datetime.utcnow()
+    return Response(f"Notification API (UTC: {current_time.strftime('%Y.%m.%d %H:%M:%S')})")
